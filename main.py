2ed10746088b50d699fb3f862a031f3f38e176f1
mohammadSelimReza <srreza1999@gmail.com>
2024-04-29 10:38:47 +0600

    some class added

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Welcome To Sirajgong Hotel")
+# common class:
+from abc import ABC
+
+class User(ABC):
+    def __init__(self,name,phone,email,address):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.address = address
+
+# Employee class 
+class Employee(User):
+        def __init__(self, name, phone, email, address, age, designation, salary):
+             super().__init__(name, phone, email, address)
+             self.age = age
+             self.designation = designation
+             self.salary = salary
+
+
