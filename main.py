18998103961ffb87a0b0f2b6ceddb935dd226733
mohammadSelimReza <srreza1999@gmail.com>
2024-05-03 08:07:35 +0600

    classes added

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-# common class:
+#common class for Customer , Admin, Employee
+#for reusable code use:
+#abstract base class
 from abc import ABC
 
+#class
 class User(ABC):
-    def __init__(self,name,phone,email,address):
+    #constructor:
+    def __init__(self,name,email,phone,address):
         self.name = name
-        self.phone = phone
         self.email = email
+        self.phone = phone
         self.address = address
-
-# Employee class 
+        
+#employee class:
 class Employee(User):
-        def __init__(self, name, phone, email, address, age, designation, salary):
-             super().__init__(name, phone, email, address)
-             self.age = age
-             self.designation = designation
-             self.salary = salary
+    def __init__(self, name, email, phone, address,age,position,salary):
+        super().__init__(name, email, phone, address)
+        self.age = age
+        self.position = position
+        self.salary = salary
+        
+#Admin:
+class Admin(User):
+    def __init__(self, name, email, phone, address):
+        super().__init__(name, email, phone, address)
+        
+        
+#Customer:
+class Customer(User):
+    def __init__(self, name, email, phone, address):
+        super().__init__(name, email, phone, address)
 
 
+
+#test:
+emp = Employee("Sohan Sheikh","ssheikha2024@gmail.com","+8801523541698","Badda",16,"Sub worker",9000)
+print(emp.name,emp.position,emp.salary)
